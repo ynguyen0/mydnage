80cae7ed4b01797497529e284082eaf19d772860
clemygw <clemygw@gmail.com>
2025-06-05 13:53:14 -0700

    added tailwind to index.css

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import TestColors from './TestColors'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <TestColors />
+      <div className="bg-harmony-500 text-white p-4 rounded">
+        Custom colour works!
+      </div>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
