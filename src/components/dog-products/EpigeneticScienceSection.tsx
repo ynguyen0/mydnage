4dc03ad02cb2a9557445312bc57f1f24c1ee7cdf
Ynha Nguyen <ynhapnguyen@gmail.com>
2025-06-09 11:35:59 -0700

    dog page done

--- src/components/dog-products/EpigeneticScienceSection.tsx
+++ src/components/dog-products/EpigeneticScienceSection.tsx
@@ -3,9 +3,7 @@ export default function EpigeneticScienceSection() {
   return (
     <div className="text-center">
       <h2 className="text-3xl md:text-4xl font-bold text-[#2A5987] mb-8 leading-tight max-w-4xl mx-auto">
-        By leveraging cutting-edge epigenetic science, LIV provides tools that
-        help people make informed decisions to enhance their health and
-        longevity.
+        By leveraging cutting-edge DNA science, Liv helps pet owners enhance their dog's health and longevity.
       </h2>
     </div>
   );
--- src/components/dog-products/TrueAgeSection.tsx
+++ src/components/dog-products/TrueAgeSection.tsx
@@ -1,15 +1,16 @@
-import { Link } from "react-router-dom";
+
 import smiling from "../../assets/smiling-lady.png";
-import dna from "../../assets/age stat.png"
+import dna from "../../assets/dog-dna-stat.png"
+import dog from "../../assets/Dog.png"
 
 // src/components/products/TrueAgeSection.tsx
 export default function TrueAgeSection() {
   return (
     <div>
       <h2 className="text-4xl md:text-5xl lg:text-6xl font-bold text-[#393D43] mb-8 leading-tight">
-        Unlocking Your TrueAge to Live
+        Your dog can't say how old they
         <br />
-        Longer, Better
+        feel, but their DNA can
       </h2>
 
 
@@ -17,19 +18,14 @@ export default function TrueAgeSection() {
         {/* Left column */}
         <div className="space-y-6">
           <p className="text-lg text-[#393D43]/70 leading-relaxed">
-            Chronological age is the number of years you have been alive,
-            whereas biological age—your TrueAge—is the age of the cells in your
-            body.
+            myDogDNAge unlocks healthier aging for your dog. It reveals their biological age—or TrueAge—which can be different than the number of years they've been cuddled up on your couch. Their cells, as revealed through various biomarkers, could be older or younger than their actual age, and this impacts how well they sit, stay, and enjoy life as your BFF (best furry friend).
           </p>
           <p className="text-lg text-[#393D43]/70 leading-relaxed">
-            Your cells, as revealed through various biomarkers, could be older
-            or younger than your actual age, and this impacts how well your body
-            functions.
+            myDogDNAge doesn’t just measure a dog’s lifespan, it serves as a blueprint for age-appropriate, individualized care for your dog.
           </p>
           <p className="text-lg text-[#393D43]/70 font-semibold leading-relaxed">
-            MyDNAge doesn't just measure aging, but to help you optimize and
-            improve your aging trajectory for years to come.
-          </p>
+            Armed with information about what’s happening at a molecular level, myDogDNAge allows pet owners to adjust diet, exercise and socialization activities, supplement intake, and veterinary care through every stage of their dog’s life to increase their happiness and longevity.
+                      </p>
         </div>
 
         {/* Right column: image + text */}
@@ -42,17 +38,17 @@ export default function TrueAgeSection() {
       "
           >
             <img
-              src={smiling}
-              alt="Woman smiling"
+              src={dog}
+              alt="Happy dog"
               className="w-full h-full object-cover"
             />
              <img
-  src={dna}
-  alt="DNA overlay"
-  className="
-    absolute inset-0
-    w-full h-full 
-    translate-y-18
+            src={dna}
+            alt="DNA overlay"
+              className="
+                absolute inset-0
+                w-full h-full 
+    translate-y-25
     -translate-x-10
     scale-110
     object-contain
@@ -60,32 +56,10 @@ export default function TrueAgeSection() {
   "
 />
           </div>
+            
 
-          {/* 2) “learning what is…” text */}
-          <div className="max-w-sm self-start text-left pt-8">
-            <p className="text-base font-semibold text-[#4F7FA8] leading-relaxed mb-4">
-              Learning what is happening in your body at a DNA level allows you
-              to unlock potential health concerns at the source and make more
-              informed lifestyle choices to increase your health longevity.
-            </p>
-            <Link to="/our-science">
-              <button
-                type="button"
-                className="
-            mt-3 inline-flex items-center gap-3
-            rounded-full bg-[#393D43] px-6 py-3
-            text-lg font-semibold text-white shadow-md
-            transition-colors hover:bg-[#2e3237]
-            focus:outline-none
-          "
-              >
-                <span className="text-[#AFD5E7]">Our Science</span>
-              </button>
-            </Link>
           </div>
         </div>
       </div>
-
-    </div>
-  );
+      );
 }
